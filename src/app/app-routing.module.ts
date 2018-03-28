82057fad3245ed2a6b8cfbd0715fda5733ccaf74
Onodric <onodric@yahoo.com>
2018-03-27 22:16:38 -0500

    heros works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PathnotfoundComponent } from './pathnotfound/pathnotfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'heros', loadChildren: 'app/heros/heros.module#HerosModule' },
+  { path: '**', pathMatch: 'full', component: PathnotfoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
